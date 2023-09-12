e0e929f30c2a3694b27609278674e163c4120b73
10672436 <harsha.devnani@external.nexigroup.com>
2023-09-13 02:41:20 +0530

    Basic Validations added

--- app/src/main/java/com/example/matchmate/viewmodel/InformationViewModel.kt
+++ app/src/main/java/com/example/matchmate/viewmodel/InformationViewModel.kt
@@ -14,64 +14,62 @@ import kotlinx.coroutines.launch
 
 class InformationViewModel(private val repository: ResultsRepository) : ViewModel() {
 
-
-    private val cardModelTransformationList =
-        ArrayList<CardModelTransformation>()//Creating an empty arraylist
-    private val loaderManager =LoaderManager()
-
-
+    private val cardModelTransformationList = ArrayList<CardModelTransformation>()
+    private val loaderManager = LoaderManager()
     var recyclerListLiveData: MutableLiveData<DataManager> = MutableLiveData()
-
+    var errorLiveData: MutableLiveData<String> = MutableLiveData()
 
     fun getRecyclerListObserver(): MutableLiveData<DataManager> {
-
         return recyclerListLiveData
     }
 
     fun makeApiCall() {
         loaderManager.showLoader()
-        viewModelScope.launch(Dispatchers.IO)
-        {
-            val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
-            val response = retroInstance.fetchData(10)
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
+                val response = retroInstance.fetchData(10)
 
+                if (response.isSuccessful) {
+                    for (items in response.body()?.results ?: emptyList()) {
+                        cardModelTransformationList.add(
+                            CardModelTransformation(
+                                items.name.first,
+                                items.name.last,
+                                items.location.street.number,
+                                items.location.country,
+                                items.picture.large
+                            )
+                        )
+                    }
 
-            for (items in response.body()!!.results) {
-                cardModelTransformationList.add(
-                    CardModelTransformation(
-                        items.name.first,
-                        items.name.last,
-                        items.location.street.number,
-                        items.location.country,
-                        items.picture.large
-                    )
-                )
+                    val dataManager = DataManager(cardModelTransformationList)
+                    recyclerListLiveData.postValue(dataManager)
+                    loaderManager.hideLoader()
 
+                    for (items in cardModelTransformationList) {
+                        repository.insertResult(items)
+                    }
+                } else {
+                    errorLiveData.postValue("API call failed: ${response.message()}")
+                    loaderManager.hideLoader()
+                }
+            } catch (e: Exception) {
+                errorLiveData.postValue("An error occurred: ${e.message}")
+                loaderManager.hideLoader()
             }
-
-            val dataManager = DataManager(cardModelTransformationList)
-            recyclerListLiveData.postValue(dataManager)
-            loaderManager.hideLoader()
-
-
-
-            for (items in cardModelTransformationList) {
-                repository.insertResult(items)
-            }
-
-
         }
-
     }
 
     fun loadFromRepository() {
         viewModelScope.launch(Dispatchers.IO) {
-
-            val resultsEntity = repository.getAllResults()
-            val dataManager = DataManager(resultsEntity as ArrayList<CardModelTransformation>)
-            recyclerListLiveData.postValue(dataManager)
+            try {
+                val resultsEntity = repository.getAllResults()
+                val dataManager = DataManager(resultsEntity as ArrayList<CardModelTransformation>)
+                recyclerListLiveData.postValue(dataManager)
+            } catch (e: Exception) {
+                errorLiveData.postValue("Error loading data from repository: ${e.message}")
+            }
         }
     }
-
-
 }
